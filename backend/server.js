c07c96995909a1ffcb0c0940b33fadd0852e4d94
Avinash12122002 <akak2805034@gmail.com>
2026-06-23 16:06:33 +0530

    change frontend domain name

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.use(helmet());
 app.use(
   cors({
     origin: [
-      "https://tms-frontend.tmsvisa.com",
+      "https://app.tmsvisa.com/",
       "https://tmsvisa.com",
       "https://www.tmsvisa.com",
       "http://localhost:5173",
