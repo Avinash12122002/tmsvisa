b9451451a057c1109ea726a2e65c21825739e8b7
Avinash12122002 <akak2805034@gmail.com>
2026-06-23 15:04:30 +0530

    cors 5

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.use(helmet());
 app.use(
   cors({
     origin: [
-      "https://tms-frontend.tmsvisa.com/api",
+      "https://tms-frontend.tmsvisa.com/",
       "https://tmsvisa.com",
       "https://www.tmsvisa.com",
       "http://localhost:5173",
